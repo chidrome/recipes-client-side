5ee01fb10eed263761ed1d6bca466036a18d872b
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-13 13:38:14 -0700

    about me component working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import AboutUs from './components/AboutUs'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <AboutUs/>
     </div>
   );
 }
