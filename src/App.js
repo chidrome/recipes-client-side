669c1842a6cbea7e2ded38bc9b564fad47f0cfe8
chidrome <chidrome@gmail.com>
2019-05-13 15:19:04 -0700

    updated styling for the image carousel

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../src/scss/core.scss';
-
-import AboutUs from './components/AboutUs'
-=======
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
->>>>>>> b978a53fd1948f2ce6f6b71b6e0a716e705a3e06
 import './App.css';
 
 function App() {
