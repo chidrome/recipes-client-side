572a99328dce661c565ce01d2cb9c36fc0a1e705
chidrome <chidrome@gmail.com>
2019-05-13 14:12:14 -0700

    added image carousel on the front page. removed serviceworker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import AboutUs from './components/AboutUs'
+import AboutUs from './components/AboutUs';
+import Carousel from './components/ImgCarousel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AboutUs/>
+      <Carousel />
+      <AboutUs />
     </div>
   );
 }
