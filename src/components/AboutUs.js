d9eef9eb878915d49d677207622287b098443c1d
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-13 16:47:57 -0700

    added nav bar

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,8 +6,9 @@ import Navbar from './Navbar';
 class AboutUs extends Component {
   render() {
     return (
-      <div className = 'aboutUsDiv'>
+      <div>
       <Navbar />
+      <div className = 'aboutUsDiv'>
       <section className = 'aboutPaolo'>
         <img className = 'ourImage' src={`https://ca.slack-edge.com/T039KG69K-UH2U28Z1N-62a68e9133e7-1024`} alt={`Paolo`}/>
             <p className = 'paoloPara'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus nulla vitae lectus consectetur venenatis. Ut ut placerat magna. Phasellus aliquam tristique lectus in blandit. Sed leo lacus, consequat a consequat vitae, suscipit vitae est. Quisque at tincidunt tellus, a tincidunt urna. In ut nisi tortor. Fusce auctor sem sem. Mauris ac arcu id lectus porttitor interdum at vitae augue. Nunc elementum convallis diam ut auctor. Maecenas sit amet commodo velit, nec mollis urna. Suspendisse potenti. Etiam velit lacus, venenatis ut dui vel, porta scelerisque orci.</p>
@@ -30,6 +31,7 @@ class AboutUs extends Component {
             <p className = 'sudipPara'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dapibus nulla vitae lectus consectetur venenatis. Ut ut placerat magna. Phasellus aliquam tristique lectus in blandit. Sed leo lacus, consequat a consequat vitae, suscipit vitae est. Quisque at tincidunt tellus, a tincidunt urna. In ut nisi tortor. Fusce auctor sem sem. Mauris ac arcu id lectus porttitor interdum at vitae augue. Nunc elementum convallis diam ut auctor. Maecenas sit amet commodo velit, nec mollis urna. Suspendisse potenti. Etiam velit lacus, venenatis ut dui vel, porta scelerisque orci.</p>
         </section>
         
+      </div>
       </div>
     )
   }
