ebcb7d5213be5a7df6823394010d7038d13b4658
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-15 14:46:12 -0700

    more details page set up

--- src/components/ResultCards.js
+++ src/components/ResultCards.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { Link} from 'react-router-dom';
 // import MoreDetails from './MoreDetails';
 
+
+
 export default class ResultCards extends Component {
+
+  resultClickHandler = (e) => {
+    e.preventDefault();
+    console.log('recipe', this.props.recipe);
+    this.props.handleClickMoreDetails(this.props.recipe); 
+  }
+
+  
   render() {
     return (
       <div>
         <img src={this.props.recipe.image} alt=""/>
         <h1>{this.props.recipe.label}</h1>
-        <button className = 'menuItem'><a href="/moreDetails">More Details</a></button>
+        <button onClick = {this.resultClickHandler} className = 'menuItem'><Link to="/moreDetails">More Details</Link></button>
         {/* Insert code for the more details link. For some reason I can't think of the logic for it this early in the morning. */}
         {/* <a href="/moreDetails"><MoreDetails recipeInfo={this.props.recipe} /></a> */}
       </div>
