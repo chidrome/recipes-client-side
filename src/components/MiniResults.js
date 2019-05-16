59c47340b5668fc90fc471da9202a730fd59a519
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-16 13:48:36 -0700

    show all results only if data exists

--- src/components/MiniResults.js
+++ src/components/MiniResults.js
@@ -24,7 +24,6 @@ export default class MiniResults extends Component {
           {FiveResults}
           {this.state.results ? (<Link to="/allResults">See All Results</Link>) : ''}
         </div>
-      
     )
   }
 }
