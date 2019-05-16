ea85c056d118b1fd99eaf86822d0b145fd7eaebe
jshin83 <jen.kyongah.lee@gmail.com>
2019-05-16 14:00:48 -0700

    fixed logice in MiniResults for see all link

--- src/components/MiniResults.js
+++ src/components/MiniResults.js
@@ -12,17 +12,20 @@ export default class MiniResults extends Component {
 
   render() {
     let FiveResults;
+    let seeAllLink;
     if(this.props.recipe) {
       FiveResults = this.props.recipe.slice(0,5).map((item, i) => {
-        this.setState({results: true})
        return <ResultCards handleClickMoreDetails = {this.props.handleClickMoreDetails} recipe={item} key = {i}/>
      })
+      if(this.props.recipe.length > 0) {
+        seeAllLink = this.props.recipe ? (<Link to="/allResults">See All Results</Link>) : ''
+      }
     }
     return (
 
         <div className='miniResult'>
           {FiveResults}
-          {this.state.results ? (<Link to="/allResults">See All Results</Link>) : ''}
+          {seeAllLink}
         </div>
     )
   }
