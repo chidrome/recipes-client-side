6270e5d5153dda569da91d745b258cda19db4d30
chidrome <chidrome@gmail.com>
2019-05-16 09:40:40 -0700

    changed the navbar links to Link rather than anchor tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,10 +18,10 @@ export default class Navbar extends Component {
         <div>
           <nav className="navbar">
             <div className="nav-wrapper">
-              <a href="/"><FontAwesomeIcon className = 'homeIcon' icon={faHome} /></a>
+              <Link to="/"><FontAwesomeIcon className = 'homeIcon' icon={faHome} /></Link>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li className = 'menuItem'><a href="/">Home</a></li>
-                <li className = 'menuItem'><a href="/aboutus">About Us</a></li>
+                <li className = 'menuItem'><Link to="/">Home</Link></li>
+                <li className = 'menuItem'><Link to="/aboutus">About Us</Link></li>
                 <li className = 'menuItem'><Link to="/profile">Profile</Link></li>
                 <li><button className="logout-button" onClick={this.handleLogout}><Link to="/">Logout</Link></button></li>
                 
