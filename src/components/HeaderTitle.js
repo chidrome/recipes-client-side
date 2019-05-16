50807a9e995dbb1a6ce045682cfdadd547a13fe8
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-05-16 09:36:13 -0700

    worked on css details

--- src/components/HeaderTitle.js
+++ src/components/HeaderTitle.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react'
 export default class HeaderTitle extends Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
+      <div className = 'header'>
+        {/* <h1>{this.props.title}</h1> */}
+        <h1>Know Your Diet</h1>
       </div>
     )
   }
